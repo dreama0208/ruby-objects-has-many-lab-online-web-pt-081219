d55b605fcc1eedc7f986b9ebd0065207a5f40f13
imperative-genre-4923 <dreama.lin@yale.edu>
2019-09-20 14:43:14 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -9,4 +9,13 @@ class Author
   def posts
     Post.all.select { |post| post == self.name }
   end
+
+  def add_post(post)
+    post.author = self
+  end
+
+  def add_post_by_title(title)
+    post = Post.new(title)
+    add_post(post)
+  end
 end
